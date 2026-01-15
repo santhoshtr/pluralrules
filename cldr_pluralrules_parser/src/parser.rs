b4c8adac1e177b223682473ce1988ab915f38b0e
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2026-01-15 16:23:50 +0530

    Migrate cldr_pluralrules_parser to nom 8.0 API
    Update parser.rs to use nom 8.0's new parser combinator API:
    - Replace deprecated 'separated_list' with 'separated_list0'
    - Replace deprecated 'separated_nonempty_list' with 'separated_list1'
    - Remove redundant 'tuple()' wrappers (tuples implement Parser directly)
    - Update all parser functions to use '.parse(input)' calling convention
    - Add 'Parser' trait import for the new method API
    
    All 48 tests pass with the new nom 8.0 API.

--- cldr_pluralrules_parser/src/parser.rs
+++ cldr_pluralrules_parser/src/parser.rs
@@ -1,17 +1,18 @@
 use super::ast::*;
 use nom::{
     IResult,
+    Parser,
     branch::alt,
     //error::context,
     bytes::complete::tag,
     character::complete::{digit1, one_of, space0, space1},
     combinator::{map, map_res, opt},
-    multi::{separated_list, separated_nonempty_list},
-    sequence::{preceded, separated_pair, tuple},
+    multi::{separated_list0, separated_list1},
+    sequence::{preceded, separated_pair},
 };
 
 fn value(i: &str) -> IResult<&str, Value> {
-    map_res(digit1, |s: &str| s.parse::<usize>().map(Value))(i)
+    map_res(digit1, |s: &str| s.parse::<usize>().map(Value)).parse(i)
 }
 
 fn range(i: &str) -> IResult<&str, Range> {
@@ -21,21 +22,24 @@ fn range(i: &str) -> IResult<&str, Range> {
             lower_val,
             upper_val,
         },
-    )(i)
+    )
+    .parse(i)
 }
 
 fn range_list_item(i: &str) -> IResult<&str, RangeListItem> {
     alt((
         map(range, RangeListItem::Range),
         map(value, RangeListItem::Value),
-    ))(i)
+    ))
+    .parse(i)
 }
 
 fn range_list(i: &str) -> IResult<&str, RangeList> {
     map(
-        separated_list(tuple((space0, tag(","), space0)), range_list_item),
+        separated_list0((space0, tag(","), space0), range_list_item),
         RangeList,
-    )(i)
+    )
+    .parse(i)
 }
 
 fn operand(i: &str) -> IResult<&str, Operand> {
@@ -47,27 +51,31 @@ fn operand(i: &str) -> IResult<&str, Operand> {
         'f' => Operand::F,
         't' => Operand::T,
         _ => unreachable!(),
-    })(i)
+    })
+    .parse(i)
 }
 
 fn mod_expression(i: &str) -> IResult<&str, Option<Modulo>> {
     opt(map(
-        preceded(tuple((space0, alt((tag("mod"), tag("%"))), space1)), value),
+        preceded((space0, alt((tag("mod"), tag("%"))), space1), value),
         Modulo,
-    ))(i)
+    ))
+    .parse(i)
 }
 
 fn expression(i: &str) -> IResult<&str, Expression> {
-    map(tuple((operand, mod_expression)), |(operand, modulus)| {
-        Expression { operand, modulus }
-    })(i)
+    map((operand, mod_expression), |(operand, modulus)| Expression {
+        operand,
+        modulus,
+    })
+    .parse(i)
 }
 
 fn relation_operator(i: &str) -> IResult<&str, Operator> {
     alt((
         map(tag("="), |_| Operator::EQ),
         map(tag("!="), |_| Operator::NotEQ),
-        map(tuple((tag("is"), space1, opt(tag("not")))), |(_, _, n)| {
+        map((tag("is"), space1, opt(tag("not"))), |(_, _, n)| {
             if n.is_some() {
                 Operator::IsNot
             } else {
@@ -76,86 +84,86 @@ fn relation_operator(i: &str) -> IResult<&str, Operator> {
         }),
         map(tag("in"), |_| Operator::In),
         map(
-            tuple((
+            (
                 tag("not"),
                 space1,
                 alt((
                     map(tag("in"), |_| Operator::NotIn),
                     map(tag("within"), |_| Operator::NotWithin),
                 )),
-            )),
+            ),
             |(_, _, v)| v,
         ),
         map(tag("within"), |_| Operator::Within),
-    ))(i)
+    ))
+    .parse(i)
 }
 
 fn relation(i: &str) -> IResult<&str, Relation> {
     map(
-        tuple((expression, space0, relation_operator, space0, range_list)),
+        (expression, space0, relation_operator, space0, range_list),
         |(expression, _, operator, _, range_list)| Relation {
             expression,
             operator,
             range_list,
         },
-    )(i)
+    )
+    .parse(i)
 }
 
 fn and_condition(i: &str) -> IResult<&str, AndCondition> {
     map(
-        separated_nonempty_list(tuple((space1, tag("and"), space1)), relation),
+        separated_list1((space1, tag("and"), space1), relation),
         AndCondition,
-    )(i)
+    )
+    .parse(i)
 }
 
 fn decimal_value(i: &str) -> IResult<&str, DecimalValue> {
     map(
-        tuple((value, opt(preceded(tag("."), value)))),
+        (value, opt(preceded(tag("."), value))),
         |(integer, decimal)| DecimalValue { integer, decimal },
-    )(i)
+    )
+    .parse(i)
 }
 
 fn sample_range(i: &str) -> IResult<&str, SampleRange> {
     map(
-        tuple((
+        (
             decimal_value,
-            opt(preceded(tuple((space0, tag("~"), space0)), decimal_value)),
-        )),
+            opt(preceded((space0, tag("~"), space0), decimal_value)),
+        ),
         |(lower_val, upper_val)| SampleRange {
             lower_val,
             upper_val,
         },
-    )(i)
+    )
+    .parse(i)
 }
 
 fn sample_list(i: &str) -> IResult<&str, SampleList> {
     map(
-        tuple((
-            separated_nonempty_list(tuple((space0, tag(","), space0)), sample_range),
+        (
+            separated_list1((space0, tag(","), space0), sample_range),
             opt(preceded(
-                tuple((space0, tag(","), space0)),
+                (space0, tag(","), space0),
                 alt((tag("..."), tag("…"))),
             )),
-        )),
+        ),
         |(l, ellipsis)| SampleList {
             sample_ranges: l,
             ellipsis: ellipsis.is_some(),
         },
-    )(i)
+    )
+    .parse(i)
 }
 
 fn samples(i: &str) -> IResult<&str, Option<Samples>> {
     map(
-        tuple((
-            opt(preceded(
-                tuple((space1, tag("@integer"), space1)),
-                sample_list,
-            )),
-            opt(preceded(
-                tuple((space1, tag("@decimal"), space1)),
-                sample_list,
-            )),
-        )),
+        (
+            opt(preceded((space1, tag("@integer"), space1), sample_list)),
+            opt(preceded((space1, tag("@decimal"), space1), sample_list)),
+        ),
         |(integer, decimal)| {
             if integer.is_some() || decimal.is_some() {
                 Some(Samples { integer, decimal })
@@ -163,13 +171,16 @@ fn samples(i: &str) -> IResult<&str, Option<Samples>> {
                 None
             }
         },
-    )(i)
+    )
+    .parse(i)
 }
 
 pub fn parse_rule(i: &str) -> IResult<&str, Rule> {
-    map(tuple((parse_condition, samples)), |(condition, samples)| {
-        Rule { condition, samples }
-    })(i)
+    map((parse_condition, samples), |(condition, samples)| Rule {
+        condition,
+        samples,
+    })
+    .parse(i)
 }
 
 pub fn parse_condition(i: &str) -> IResult<&str, Condition> {
@@ -182,7 +193,8 @@ pub fn parse_condition(i: &str) -> IResult<&str, Condition> {
         return IResult::Ok(("", Condition(vec![])));
     }
     map(
-        separated_nonempty_list(tuple((space1, tag("or"), space1)), and_condition),
+        separated_list1((space1, tag("or"), space1), and_condition),
         Condition,
-    )(i)
+    )
+    .parse(i)
 }
